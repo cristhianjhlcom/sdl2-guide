6d30ebbe607cb0fc313ca9ae65dfeaee1d62f3a7
cristhianjhl.com <cristhianjhl.web@gmail.com>
2025-01-28 09:12:02 -0500

    add mouse events exercise to know how the mouse events works with SDL2 library

---
+++ mouse_events/include/common.h
@@ -0,0 +1,28 @@
+#pragma once
+
+// SDL2 Library.
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_blendmode.h>
+#include <SDL2/SDL_error.h>
+#include <SDL2/SDL_events.h>
+#include <SDL2/SDL_hints.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_keycode.h>
+#include <SDL2/SDL_pixels.h>
+#include <SDL2/SDL_rect.h>
+#include <SDL2/SDL_render.h>
+#include <SDL2/SDL_stdinc.h>
+#include <SDL2/SDL_surface.h>
+#include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL_video.h>
+
+// Standard libraries.
+#include <stdbool.h>
+#include <stdio.h>
+#include <time.h>
+
+// Custom headers.
+#include "constants.h"
+#include "game_state.h"
+#include "setup.h"
+#include "texture.h"
---
+++ mouse_events/src/main.c
@@ -0,0 +1,67 @@
+#include "common.h"
+
+game_state_t game_state;
+
+int main(void) {
+    memset(&game_state, 0, sizeof(game_state_t));
+
+    if (!init()) {
+        printf("App Initialization failed!\n");
+        exit(1);
+    }
+
+    texture_init(&text_texture);
+
+    if (!load_media()) {
+        printf("Load media image failed %s\n", SDL_GetError());
+        exit(1);
+    }
+
+    game_state.is_running = true;
+
+    // SDL Event handle system.
+    // - Key press. (SDL_KeyboardEvent)
+    // - Mouse motion. (SDL_MouseMotionEvent)
+    // - Joy button press. (SDL_JoyButtonEvent)
+    SDL_Event event;
+
+    // This is the game loop.
+    // The core of any game application.
+    while (game_state.is_running) {
+        // Event loop.
+        // Handle events on queue.
+        // Keep proccesing the event queue until it is empty.
+        while (SDL_PollEvent(&event)) {
+            switch (event.type) {
+                // User requests quit the game.
+                case SDL_QUIT:
+                    game_state.is_running = false;
+                    break;
+            }
+        }
+
+        // Set clearing white color on every frame.
+        // Instead of set once on the init function.
+        // Explained later!
+        SDL_SetRenderDrawColor(game_state.renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+        // Clear screen.
+        // Fills the screen we the color that we define on init function
+        // - SDL_SetRenderDrawColor
+        SDL_RenderClear(game_state.renderer);
+
+        // Element x and y positions.
+        int x_pos = (SCREEN_WIDTH - text_texture.w) / 2;
+        int y_pos = (SCREEN_HEIGHT - text_texture.h) / 2;
+
+        // Blit here.
+        texture_render(&text_texture, x_pos, y_pos, NULL, 0.0, NULL, SDL_FLIP_NONE);
+
+        // Now we have to user RenderPresent because we are not using
+        // surface anymore then update screen.
+        SDL_RenderPresent(game_state.renderer);
+    }
+
+    cleanup();
+
+    return EXIT_SUCCESS;
+}
